cb911bf81e63aa8b5960691fa47d1736ab1caf52
obadia <obadia.maasai@student.moringaschool.com>
2023-07-24 12:34:57 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqBlocks(someValue){
+    const = ("distace")
+    if (someValue>=42){
+
+    }else{
+        return 42- someValue
+    }
+}
+function distanceTravelledInFeet(start, destination){
+    if (destination>=start){
+        return (destination-start)*264
+    }else{
+
+    }
+}
+
+
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqBlocks(someValue)*264
+
+}
+function calculatesFarePrice(someValue ){
+    if(distanceTravelledInFeet<=400){
+        return 0}
+         else  if (distanceTravelledInFeet >400 && distanceTravelledInFeet<=2000)
+  //return(distanceTravelledInFeet-400) * 0.02}
+
+     if(distanceTravelledInFeet>2000 && distanceTravelledInFeet<=2500){
+    return 25}
+    else {
+        return "cannot travel that far"
+    }
+   
+}
